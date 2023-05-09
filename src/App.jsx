49bed4052ce430f2fb7bb499a06c52ac13188b1e
Gauri211 <114096020+Gauri211@users.noreply.github.com>
2023-05-09 17:12:17 +0530

    Testimonials added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './layout/Login'
 import Footer from './layout/Footer/Footer'
+import Testimonials from './layout/Testimonials/Testimonials'
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path='/footer' element={<> <Footer/> </>}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/testimonials' element={<> <Testimonials/> </>}></Route>
         </Routes>
       </Router>
     </div>

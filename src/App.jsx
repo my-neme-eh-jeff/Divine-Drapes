988f6c0478f93802af17e54e5834e52662cc7857
vatsalshah041 <shahvatsal2003@gmail.com>
2023-05-13 12:04:47 +0530

    Profile started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './layout/Login'
 import Footer from './layout/Footer/Footer'
+import Profile from './layout/Profile'
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path='/footer' element={<> <Footer/> </>}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/profile' element={<><Profile/></>}></Route>
         </Routes>
       </Router>
     </div>

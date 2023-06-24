56ee8cb9223ae0231e2f590117614e4bc5cacc70
vatsalshah041 <shahvatsal2003@gmail.com>
2023-06-24 13:44:25 +0530

    view changes

--- src/layout/Myorders.jsx
+++ src/layout/Myorders.jsx
@@ -53,8 +53,8 @@ export default function Myorders() {
             <Grid item md={12} xs={12} sx={{ marginTop: "3px" }}>
               My Orders
             </Grid>
-            <Grid item md={2.4} xs={12}>
-              <Card sx={{ maxWidth: 200 }}>
+            <Grid item md={2.4} xs={12} sm={12} sx={{justifyContent:"center",display:"flex",alignItems:"center"}}>
+              <Card sx={{ maxWidth: 250,"@media (min-width:700px)":{maxWidth:200} }} className="cards">
                 <CardMedia
                   component="img"
                   alt="green iguana"
@@ -107,8 +107,8 @@ export default function Myorders() {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item md={2.4} xs={12}>
-              <Card sx={{ maxWidth: 200 }}>
+            <Grid item md={2.4} xs={12} sm={12} sx={{justifyContent:"center",display:"flex",alignItems:"center"}}>
+              <Card sx={{ maxWidth: 250,"@media (min-width:700px)":{maxWidth:200} }}>
                 <CardMedia
                   component="img"
                   alt="green iguana"
@@ -160,8 +160,8 @@ export default function Myorders() {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item md={2.4} xs={12}>
-              <Card sx={{ maxWidth: 200 }}>
+            <Grid item md={2.4} xs={12} sm={12} sx={{justifyContent:"center",display:"flex",alignItems:"center"}}>
+              <Card sx={{ maxWidth: 250,"@media (min-width:700px)":{maxWidth:200} }}>
                 <CardMedia
                   component="img"
                   alt="green iguana"
@@ -213,61 +213,8 @@ export default function Myorders() {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item md={2.4} xs={12}>
-              <Card sx={{ maxWidth: 200 }}>
-                <CardMedia
-                  component="img"
-                  alt="green iguana"
-                  height="200"
-                  image={mug}
-                  sx={{
-                    borderTopLeftRadius: "10px",
-                    borderTopRightRadius: "10px",
-                    borderBottomRightRadius: "10px",
-                    borderBottomLeftRadius: "10px",
-                  }}
-                />
-                <CardContent sx={{}}>
-                  <Typography gutterBottom variant="h7" component="div" sx={{}}>
-                    Mug
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    some decription...
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    size="small"
-                    variant="contained"
-                    sx={{
-                      color: "white",
-                      backgroundColor: "#A01E86",
-                      "&:hover": { backgroundColor: "#A01E86", border: 2 },
-                    }}
-                  >
-                    View
-                  </Button>
-                  <Button
-                    size="small"
-                    variant="outlined"
-                    sx={{
-                      borderColor: "#A01E86",
-                      color: "#A01E86",
-                      border: 2,
-                      "&:hover": {
-                        backgroundColor: "white",
-                        borderColor: "#A01E86",
-                        border: 2,
-                      },
-                    }}
-                  >
-                    Comment
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-            <Grid item md={2.4} xs={12}>
-              <Card sx={{ maxWidth: 200 }}>
+            <Grid item md={2.4} xs={12} sm={12} sx={{justifyContent:"center",display:"flex",alignItems:"center"}}>
+              <Card sx={{ maxWidth: 250,"@media (min-width:700px)":{maxWidth:200} }}>
                 <CardMedia
                   component="img"
                   alt="green iguana"

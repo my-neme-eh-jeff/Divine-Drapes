2f394512f1ea87d6c3d73063e5b4721a0bb6a1c3
jeff <aman2003nambisan@gmail.com>
2023-07-27 16:51:31 +0530

    Added frontend domain to cors setup

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  //!apna frontend ka port daldena 
+  //!apna frontend ka port daldena
+  "https://divinedrapesfeweb.netlify.app",
   "http://127.0.0.1",
   "http://localhost:3000",
 ];

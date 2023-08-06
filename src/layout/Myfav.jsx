077dbf7191075f9da288ebcb7357a2fda3a86681
vatsalshah041 <shahvatsal2003@gmail.com>
2023-08-06 13:10:15 +0530

    add to cart del from cart done succesfully

--- src/layout/Myfav.jsx
+++ src/layout/Myfav.jsx
@@ -20,16 +20,16 @@ export default function Myorders() {
     const { auth, setAuth } = useAuth();
     const [favitem,setFavitem]=useState() 
     const delfromfav=(id)=>{
-      console.log(id);
 
       let config = {
-        method: 'post',
+        method: 'delete',
         maxBodyLength: Infinity,
         url: 'https://divine-drapes.onrender.com/user/removeCart/'+id,
         headers: { 
           'Authorization': 'Bearer '+isLogin,
         }
       };
+      console.log(config.url);
 
         async function makeRequest() {
           try {
@@ -42,6 +42,30 @@ export default function Myorders() {
         }
 
         makeRequest();
+
+    }
+    const addtofav=(id)=>{
+      console.log(id);
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://divine-drapes.onrender.com/user/addCart/'+id,
+  headers: { 
+    'Authorization': 'Bearer '+isLogin,
+  }
+};
+
+async function makeRequest() {
+  try {
+    const response = await privateAxios.request(config);
+    console.log((response.data));
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+makeRequest();
 
     }
   const isLogin = auth?.accessToken;
@@ -62,7 +86,11 @@ export default function Myorders() {
         try {
           const response = await privateAxios.request(config);
           console.log((response.data.data));
-          setFavitem(response.data.data)
+          const favData = response.data.data.map((item) => ({
+            ...item,
+            liked: true,
+          }));
+          setFavitem(favData);
 
         } catch (error) {
           console.log(error);
@@ -71,6 +99,29 @@ export default function Myorders() {
   
       makeRequest();
     }, []);
+    const toggleLike1 = (id) => {
+      setFavitem((prevFavitem) =>
+        prevFavitem.map((item) => {
+          if (item._id === id) {
+            return { ...item, liked: !item.liked };
+          }
+          return item;
+        })
+      );
+      delfromfav(id)
+    }
+    const toggleLike2 = (id) => {
+      setFavitem((prevFavitem) =>
+        prevFavitem.map((item) => {
+          if (item._id === id) {
+            return { ...item, liked: !item.liked };
+          }
+          return item;
+        })
+      );
+      addtofav(id)
+    }
+
   return (
     <>
       <Grid container>
@@ -86,6 +137,7 @@ export default function Myorders() {
             </Grid>
             {favitem?<>
             {favitem.map((item)=>{
+              console.log(item);
               return(
                 <Grid item md={2.4} xs={12} sm={12} sx={{justifyContent:"center",display:"flex",alignItems:"center"}}>
               <Card sx={{ maxWidth: 250,"@media (min-width:700px)":{maxWidth:200} }}>
@@ -98,7 +150,7 @@ export default function Myorders() {
                 />
                 <CardContent sx={{}}>
                   <Typography gutterBottom variant="h7" component="div" sx={{}}>
-                    Mug
+                    {item.name}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
                     {item.description}
@@ -107,16 +159,15 @@ export default function Myorders() {
                 <CardActions>
                   <Button size="small" variant="contained" sx={{color:"white",backgroundColor:"#A01E86","&:hover": {backgroundColor: "#A01E86",border:2 }}}>View</Button>
                   
-                  {(like==0)?<>
-                  <IconButton sx={{marginLeft:"5.5rem"}} onClick={()=>{setLike(1);delfromfav(item._id)}}>
-                    <FavoriteIcon sx={{color:"red"}}/>
-                  </IconButton>
-                    </>:<>
-                  <IconButton sx={{marginLeft:"5.5rem"}} onClick={()=>{setLike(0)}}>
-                    <FavoriteBorderIcon sx={{color:"black"}}/>
-                  </IconButton>
-                    
-                    </>}
+                  {item.liked ? (
+            <IconButton sx={{ marginLeft: "5.5rem" }} onClick={() => toggleLike1(item._id)}>
+              <FavoriteIcon sx={{ color: "red" }} />
+            </IconButton>
+          ) : (
+            <IconButton sx={{ marginLeft: "5.5rem" }} onClick={() => toggleLike2(item._id)}>
+              <FavoriteBorderIcon sx={{ color: "black" }} />
+            </IconButton>
+          )}
                   </CardActions>
               </Card>
             </Grid>
